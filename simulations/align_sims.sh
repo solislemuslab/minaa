287ba5790ab9b609a0331dcf660e391a7a899dcf
Reed Nelson <rdnelson6@wisc.edu>
2022-12-09 14:08:46 -0600

    rename minaa

--- simulations/align_sims.sh
+++ simulations/align_sims.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-for n in 10 30 50
+for n in 10 30 50 100 250 500
 do
     for p in 0.05 0.1 0.9
     do
         for i in {1..30}
         do
-            .././mna.exe ../testdata/simulated/Graph1-$n-$p-$i.csv ../testdata/simulated/Graph2-$n-$p-$i.csv
+            ../minaa.exe ../testdata/simulated/Graph1-$n-$p-$i.csv ../testdata/simulated/Graph2-$n-$p-$i.csv
         done
     done
 done
